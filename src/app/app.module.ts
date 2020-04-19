1ec2b7ea031ed9045362e71840db8986cec0b00c
Reynaldo Claros <reyiclaros@gmail.com>
2020-04-19 18:50:19 -0500

    first versión

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyCyV3r4ZjqYiaI8TJQELhREsb3c8nlWIr4",
+      libraries: ["places"],
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
